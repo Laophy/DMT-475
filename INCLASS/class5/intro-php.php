3924e6c0be49e51880438c2870b9125ffe57f3ce
Laophy <56947136+Laophy@users.noreply.github.com>
2023-02-28 18:45:14 -0600

    bootstrap

--- INCLASS/class5/intro-php.php
+++ INCLASS/class5/intro-php.php
@@ -4,8 +4,27 @@
     </head>
 
     <body>
+    <h1>What is your favorite movie?</h1>
 
-    <?php echo '<p>Hello World</p>'; ?>
-    
+    <?php 
+        $stop1 = 408;
+        $stop2 = 277;
+        $stop3 = 423;
+        $gas = 3.19;
+        $mpg = 25;
+
+        // 
+        $distance_traveled = $stop1 + $stop2 + $stop3;
+
+        $cost = $distance_traveled * $gas / $mpg;
+
+        echo "<h2>Details of my Trip</h2>";
+
+        echo "<p>The total distance of my trip is $distance_traveled</p>";
+
+        echo "<p>The cost of gas for my trip will be $" . number_format($cost, 2) . "</p>";
+
+        echo "<p>The cost of gas for my trip will be $" . (($stop1 + $stop2 + $stop3) * $gas / $mpg) . "</p>";
+    ?>
     </body>
 </html>
